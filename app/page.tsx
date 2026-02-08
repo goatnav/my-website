9c98d5400d8df0dbf88e48b4f709a16a04c1bd79
1-nav <arnavs@tamu.edu>
2026-02-08 00:08:42 -0600

    Created basic structure of left side, minimal CSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,16 @@
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+
+
 export default function Home(){
-  return <div> hello psusy</div> ;
+  return (
+  
+  <div className = "flex flex-col min-h-screen" id = "navbar"> 
+    <Hero/>
+    <Navbar/>
+  </div> 
+  
+
+);
 
 }
